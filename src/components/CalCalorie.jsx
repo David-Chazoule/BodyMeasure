05b13414ee7d1b21cf6528df0ae85d480b69ad1e
David-Chazoule <d.chazoule@gmail.com>
2023-08-08 21:40:05 +0200

    cleaning code + comment

---
+++ src/components/CalCalorie.jsx
@@ -0,0 +1,415 @@
+import React, { useState } from "react";
+import food from "../styles/img/food.png";
+import { ValidNumber } from "./Regex";
+
+export default function CalCalorie() {
+  const [age, setAge] = useState("");
+  const [size, setSize] = useState("");
+  const [weight, setWeight] = useState("");
+  const [sexe, setSexe] = useState("");
+  const [kal, setKal] = useState("");
+  const [method, setMethod] = useState("Mifflin");
+  const [fat, setFat] = useState("");
+  const [activity, setActivity] = useState("Sedentaire");
+  const [showResult, setShowResult] = useState(false);
+  const [noValidEnter, setNoValidEnter] = useState("");
+  const [errorAge, setErrorAge] = useState("");
+  const [errorSize, setErrorSize] = useState("");
+  const [errorWeight, setErrorWeight] = useState("");
+  const [errorFat, setErrorFat] = useState("");
+
+
+
+  /*  function that allows you to calculate your caloric need with the Harris method */
+
+  const methodHarris = () => {
+    if (sexe === "man") {
+      const result =
+        13.397 * Number(weight) +
+        4.799 * Number(size) -
+        5.677 * Number(age) +
+        88.362;
+      activityDay(result);
+    }
+    if (sexe === "woman") {
+      const result =
+        9.247 * Number(weight) +
+        3.098 * Number(size) -
+        4.33 * Number(age) +
+        447.593;
+      activityDay(result);
+    }
+  };
+
+
+  /*  function that allows you to calculate your caloric need with the Katch method */
+
+  const methodKatch = () => {
+    const fatcal = Number(fat) / 100;
+    const result = 370 + 21.6 * (1 - fatcal) * 63;
+    activityDay(result);
+  };
+
+  /*  function that allows you to calculate your caloric need with the Mifflin method */
+
+  const MethodMifflin = () => {
+    if (sexe === "man") {
+      const result =
+        10 * Number(weight) + 6.25 * Number(size) - 5 * Number(age) + 5;
+      activityDay(result);
+    }
+    if (sexe === "woman") {
+      const result =
+        10 * Number(weight) + 6.25 * Number(size) - 5 * Number(age) - 161;
+      activityDay(result);
+    }
+  };
+
+  /*  function which checks which method was chosen by the user */
+
+  const calculBMR = () => {
+    if (method === "Mifflin") {
+      MethodMifflin();
+    }
+
+    if (method === "Harris") {
+      methodHarris();
+    }
+    if (method === "Katch") {
+      methodKatch();
+    }
+  };
+
+  /*  function that adds a multiplier coefficient according to the physical activity of the user on the result of caloric need */
+
+  const activityDay = (x) => {
+    if (activity === "Sedentaire") {
+      const coef = x * 1.2;
+      return setKal(coef.toFixed(0));
+    }
+    if (activity === "Lg-actif") {
+      const coef = x * 1.375;
+      return setKal(coef.toFixed(0));
+    }
+    if (activity === "Actif") {
+      const coef = x * 1.55;
+      return setKal(coef.toFixed(0));
+    }
+    if (activity === "T-actif") {
+      const coef = x * 1.725;
+      return setKal(coef.toFixed(0));
+    }
+    if (activity === "Ex-actif") {
+      const coef = x * 1.9;
+      return setKal(coef.toFixed(0));
+    }
+  };
+
+  /* function that checks if the data entered by the user are indeed numbers */
+
+  const NumberVerification = () => {
+    if (!ValidNumber.test(weight)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setShowResult(false);
+      setErrorWeight("error-style");
+    } else if (!ValidNumber.test(size)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setShowResult(false);
+      setErrorSize("error-style");
+    } else if (!ValidNumber.test(age)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setShowResult(false);
+      setErrorAge("error-style");
+    } else if (method === "Katch" && !ValidNumber.test(fat)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setShowResult(false);
+      setErrorFat("error-style");
+    } else {
+      setNoValidEnter("");
+    }
+  };
+
+  const handleClick = () => {
+    setKal("");
+    setSize("");
+    setWeight("");
+    setAge("");
+    setFat("");
+    setShowResult(false);
+  };
+
+  const handlePost = (e) => {
+    e.preventDefault();
+    calculBMR();
+    setShowResult(!showResult);
+  };
+
+  const onOptionChange = (e) => {
+    setMethod(e.target.value);
+  };
+
+  const onActivityChange = (e) => {
+    setActivity(e.target.value);
+  };
+
+  return (
+    <div className="calcul_kalorie_container">
+      <form onSubmit={(e) => NumberVerification(handlePost(e))}>
+        <div className="form-container">
+          <div className="method-box">
+            <p>Methode BMR </p>
+            <div>
+              <label>
+                <input
+                  type="radio" 
+                  value="Mifflin"
+                  id="mifflin"
+                  checked={method === "Mifflin"}
+                  onChange={onOptionChange}
+                />
+                <span>Mifflin St jeor</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Harris"
+                  id="harris"
+                  checked={method === "Harris"}
+                  onChange={onOptionChange}
+                />
+                <span>Harris-Benedict</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Katch"
+                  id="katch"
+                  checked={method === "Katch"}
+                  onChange={onOptionChange}
+                />
+                <span>Katch-McArdle</span>
+              </label>
+            </div>
+          </div>
+          <div className="input-container">
+            <div className="information-container">
+              <div className="sex-box">
+                <label>
+                  <select
+                    value={sexe}
+                    required
+                    onChange={(e) => setSexe(e.target.value)}
+                  >
+                    <option value="">Sexe</option>
+                    <option value="man">Homme</option>
+                    <option value="woman">Femme</option>
+                  </select>
+                </label>
+              </div>
+              <div className="age-fat-box">
+                <label>
+                  <input
+                    type="text"
+                    placeholder="Votre age"
+                    className={noValidEnter ? errorAge : ""}
+                    value={age}
+                    required
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </label>
+
+                {method === "Katch" ? (
+                  <label>
+                    <input
+                      type="text"
+                      placeholder="% graisse corporelle"
+                      className={noValidEnter ? errorFat : ""}
+                      value={fat}
+                      required
+                      onChange={(e) => setFat(e.target.value)}
+                    />
+                  </label>
+                ) : (
+                  ""
+                )}
+              </div>
+
+              <div className="size-weigth-bodyfat">
+                <label>
+                  <input
+                    type="text"
+                    placeholder="Votre taille en cm"
+                    className={noValidEnter ? errorSize : ""}
+                    value={size}
+                    required
+                    onChange={(e) => setSize(e.target.value)}
+                  />
+                </label>
+
+                <label>
+                  <input
+                    type="text"
+                    placeholder="Votre poid en kilos"
+                    className={noValidEnter ? errorWeight : ""}
+                    value={weight}
+                    required
+                    onChange={(e) => setWeight(e.target.value)}
+                  />
+                </label>
+              </div>
+            </div>
+
+            <div className="activity-box">
+              <label>
+                <input
+                  type="radio"
+                  value="Sedentaire"
+                  id="sedentaire"
+                  checked={activity === "Sedentaire"}
+                  onChange={onActivityChange}
+                />
+                <span>Sédentaire</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Lg-actif"
+                  id="lg-actif"
+                  checked={activity === "Lg-actif"}
+                  onChange={onActivityChange}
+                />
+                <span>Légèrement actif</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="Actif"
+                  id="actif"
+                  checked={activity === "Actif"}
+                  onChange={onActivityChange}
+                />
+                <span>Actif</span>
+              </label>
+              <label>
+                <input
+                  type="radio"
+                  value="T-actif"
+                  id="t-actif"
+                  checked={activity === "T-actif"}
+                  onChange={onActivityChange}
+                />
+                <span>Très actif</span>
+              </label>
+
+              <label>
+                <input
+                  type="radio"
+                  value="Ex-actif"
+                  id="ex-actif"
+                  checked={activity === "Ex-actif"}
+                  onChange={onActivityChange}
+                />
+                <span>extrêment actif</span>
+              </label>
+            </div>
+          </div>
+          <div className="error-enter-box">
+            <span>{noValidEnter}</span>
+          </div>
+          <button type="submit">
+            {showResult ? (
+              <span onClick={handleClick}>
+                Re-calculer votre besoin calorique
+              </span>
+            ) : (
+              "Calculer votre besoin calorique"
+            )}
+          </button>
+        </div>
+      </form>
+
+      <div className="result_kal">
+        {showResult ? (
+          showResult && (
+            <div className="result-box">
+              <div className="result-title">
+                <div className="result-title-kal">
+                  <h3>Votre besoin journalier en calories </h3>
+                  <span>{kal} Calories</span>
+                </div>
+                <div className="img-box">
+                  <img src={food} alt="food" />
+                  <p>
+                    Pour conserver un poids stable, la dépense calorique
+                    quotidienne doit être égale à l'apport calorique quotidien.
+                  </p>
+                </div>
+              </div>
+
+              <div className="advice-box">
+                <h3> Comment Perdre du poid?</h3>
+                <p>
+                  vous souhaitez perdre du poids, il convient de mettre en place
+                  un déficit calorique (absorber moins de calories que vous n'en
+                  dépensez). On considère qu'il faut induire un déficit
+                  énergétique de l'ordre de <span>10 à 15 %</span>du besoin
+                  énergétique total. La diminution doit être faite
+                  progressivement, par tranche de{" "}
+                  <span>50 à 60 kcal par semaine</span>, pour ne pas brusquer
+                  l’organisme.
+                  <br />
+                  Prenez garde aux régimes restrictifs et aux déficits
+                  caloriques trop importants qui risquent d'entraîner des
+                  carences nutritionnelles, voire des mécanismes d'adaptation
+                  contre-productifs. En effet, lorsque le nombre de calories
+                  passe sous un certain seuil, l'organisme se protège et fait
+                  des réserves.
+                </p>
+              </div>
+            </div>
+          )
+        ) : (
+          <div className="intro-txt">
+            <div className="intro">
+              <p>
+                Vos besoins quotidiens en calories dépendent de votre âge, de
+                votre sexe, de votre poids, de votre taille et de votre activité
+                physique. Découvrez la formule de calcul qui permet de connaître
+                l’apport quotidien en calories recommandé pour maintenir votre
+                poids actuel.
+              </p>
+            </div>
+            <div className="methods-txt">
+              <div className="method-expl">
+                <h3>Methode Mifflin St Jeor</h3>
+                <p>
+                  Cette méthode de calcul fut introduite en 1990. Elle permet de
+                  calculer son métabolisme de base de manière précise.
+                </p>
+              </div>
+              <p></p>
+
+              <div className="method-expl">
+                <h3>Methode Harris-Benedict</h3>
+                <p>
+                  La methode Harris et Benedict de 1919 qui permet de déterminer
+                  les besoins caloriques journaliers tant pour les hommes que
+                  pour les femmes.
+                </p>
+              </div>
+
+              <div className="method-expl">
+                <h3>Methode Katch-McArdle</h3>
+                <p>
+                  Cette formule prend en compte la masse maigre de la personne
+                  ainsi que la taille.
+                </p>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/CalculImc.jsx
@@ -0,0 +1,186 @@
+import { useState } from "react";
+
+import { ValidNumber } from "./Regex";
+import body from "../styles/img/body.png";
+
+export default function CalculImc() {
+  const [weight, setWeight] = useState("");
+  const [size, setSize] = useState("");
+  const [imc, setImc] = useState("");
+  const [result, setResult] = useState(false);
+  const [colorImc, setColorImc] = useState("");
+  const [noValidEnter, setNoValidEnter] = useState("");
+  const [errorSize, setErrorSize] = useState("");
+  const [errorWeight, setErrorWeight] = useState("");
+
+  /* function to calculate BMI */
+
+  const calculImcs = () => {
+    const tailleDecimal = size / 100;
+    const result =
+      Number(weight) / (Number(tailleDecimal) * Number(tailleDecimal));
+    imcStyle(result);
+  };
+
+  /* function that checks the given imc and assigns it a color according to its level */
+
+  const imcStyle = (imc) => {
+    if (imc === 0 || imc <= 18.4) {
+      setColorImc("A");
+      return setImc(imc.toFixed(2));
+    } else if (imc === 18.5 || imc <= 24.9) {
+      setColorImc("B");
+      return setImc(imc.toFixed(2));
+    } else if (imc === 25 || imc <= 29.9) {
+      setColorImc("C");
+      return setImc(imc.toFixed(2));
+    } else if (imc === 30 || imc <= 34.9) {
+      setColorImc("D");
+      return setImc(imc.toFixed(2));
+    } else if (imc === 35 || imc <= 39.9) {
+      setColorImc("E");
+      return setImc(imc.toFixed(2));
+    } else if (imc > 40) {
+      setColorImc("F");
+      return setImc(imc.toFixed(2));
+    } else {
+      return "error";
+    }
+  };
+
+  /* function that checks if the data entered by the user are indeed numbers */
+
+  const NumberVerification = () => {
+    if (!ValidNumber.test(weight)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setResult(false);
+      setErrorWeight("error-style");
+    } else if (!ValidNumber.test(size)) {
+      setNoValidEnter("*entrée vos données en chiffres");
+      setResult(false);
+      setErrorSize("error-style");
+    } else {
+      setNoValidEnter("");
+    }
+  };
+
+  const handleClick = () => {
+    setImc("");
+    setSize("");
+    setWeight("");
+
+    setResult(false);
+  };
+
+  const handlePost = (e) => {
+    e.preventDefault();
+
+    calculImcs();
+
+    setResult(!result);
+  };
+
+  return (
+    <div className="calculator_imc_container">
+      <form onSubmit={(e) => NumberVerification(handlePost(e))}>
+        <div className="form_container">
+          <label>
+            <input
+              type="text"
+              placeholder="Votre taille en cm"
+              className={noValidEnter ? errorSize : ""}
+              value={size}
+              required
+              onChange={(e) => setSize(e.target.value)}
+            />
+          </label>
+
+          <label>
+            <input
+              type="text"
+              placeholder="Votre poid"
+              className={noValidEnter ? errorWeight : ""}
+              value={weight}
+              required
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="error-enter-box">
+          <span className="ageLimit">*Vous devez avoir entre 18 et 65 ans</span>
+          <span>{noValidEnter} </span>
+        </div>
+        <button type="submit">
+          {result ? (
+            <span onClick={handleClick}>Calculer à nouveau mon IMC </span>
+          ) : (
+            "Calculer mon IMC"
+          )}
+        </button>
+      </form>
+
+      <div className="result_imc">
+        {result ? (
+          result && (
+            <>
+              <div className="result-div">
+                <h1>Votre IMC : </h1>
+                <span className={colorImc}>{imc}</span>
+              </div>
+              <div className="summary_table">
+                <table>
+                  <thead>
+                    <tr>
+                      <th colspan="2">Indice IMC</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr>
+                      <td className="tdA"> -18,4kg/m² </td>
+                      <td>maigreur</td>
+                    </tr>
+                    <tr>
+                      <td className="tdB">entre 18,5 & 24,9kg/m²</td>
+                      <td>corpulence "normale"</td>
+                    </tr>
+                    <tr>
+                      <td className="tdC">entre 25 & 29,9kg/m²</td>
+                      <td>surpoids</td>
+                    </tr>
+                    <tr>
+                      <td className="tdD">entre 30 & 34,9kg/m²</td>
+                      <td>obésité modérée</td>
+                    </tr>
+                    <tr>
+                      <td className="tdE">entre 35 & 39,9 kg/m²</td>
+                      <td>obésidté sévère</td>
+                    </tr>
+                    <tr>
+                      <td className="tdF">au-dessus & 40kg/m²</td>
+                      <td>obésité morbide</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </>
+          )
+        ) : (
+          <>
+            <p>
+              <span>L’indice de masse corporelle</span> (body mass index ou BMI
+              en anglais) est utilisé depuis 1997 par l’Organisation mondiale de
+              la santé (OMS), principalement pour évaluer les risques liés au
+              surpoids et à l’obésité chez l’adulte de <span>18 à 65 ans</span>.
+              Cet indicateur permet de savoir si notre poids est adapté
+              proportionnellement à notre taille, ou s’il présente un danger
+              potentiel pour notre santé.
+            </p>
+
+            <img src={body} alt="body" />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
