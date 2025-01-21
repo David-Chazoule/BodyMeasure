03cb4f9bea8d6c22479bddb22eb8eadf1a0ce034
David-Chazoule <d.chazoule@gmail.com>
2025-01-21 20:17:21 +0100

    first test unitaire

---
+++ src/_test_/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+test('renders Header component', () => {
+  render(<Header />);
+  const headerElement = screen.getByText(/Body/i);  // Vérifie que "Body" est dans le header
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders logo image with correct alt text', () => {
+  render(<Header />);
+  const logoImage = screen.getByAltText(/logo/i);  // Cherche une image avec alt "logo"
+  expect(logoImage).toBeInTheDocument();
+});
